ae09f5698c552868f5bdfa9442da07ab353cee42
Tim Wu <tim.wuzhe@gmail.com>
2022-09-22 13:56:00 -0400

    Added handeling of arguments and MID list; Added usage messages.

--- io/convert_to_mrd.sh
+++ io/convert_to_mrd.sh
@@ -1,22 +1,54 @@
 #!/bin/bash 
 
-idsCartesian=(82 83)
-idsSpiral=(68 70 72 74 76 78 99 101 103 105 107 109)
+function print_usage {
+    echo "Usage: $0 -p DataPath -c CartesianMIDs -s SpiralMIDs"
+    echo "  -p DataPath             The directory of Cartesian and spiral measurement data, in double quotes. e.g. -p \"OneDrive - UHN/SPIDI003/dat\" "
+    echo "  -c CartesianMIDs        The list of measurement IDs of Cartesian data, in double quotes and separated by space. e.g. -c \"82 85 86\" "
+    echo "  -s SpiralMIDs           The list of measurement IDs of spiral data, in double quotes and separated by space. e.g. -s \"82 85 86\" "
+    echo "All arguments should be in double quotes."
+    exit 1
+}
 
-pathData="/mnt/e/SPIDI/data/SPIDI_0007/Phantom/rawdata"
-# standard conversion of .dat files
+while getopts p:c:s:h flag
+do
+    case "${flag}" in
+        p)
+            pathData="$OPTARG"
+            ;;
+        c)
+            set -f # disable globbing temporarily
+            IFS=' ' # split on space characters
+            idsCartesian=($OPTARG) # use the split+glob operator
+            ;;
+        s)
+            set -f # disable globbing temporarily
+            IFS=' ' # split on space characters
+            idsSpiral=($OPTARG)
+            ;;
+        h)
+            print_usage
+            ;;
+        *)
+            print_usage
+            ;;
+    esac
+done
+
+set +f # Turn globbing on
+
+## standard conversion of .dat files
 for id in ${idsCartesian[@]}; do
     printf -v filepattern "meas_MID%05d" "${id}"
     echo $filepattern 
-    foundFiles=$(ls ${pathData}/${filepattern}*.dat)
+    foundFiles=$(ls "${pathData}"/${filepattern}*.dat)
     echo "Converting $foundFiles to mrd using IsmrmrdParameterMap_Siemens_NX.xsl"
-    siemens_to_ismrmrd -f $foundFiles -z 2 -x IsmrmrdParameterMap_Siemens_NX.xsl --skipSyncData
+    siemens_to_ismrmrd -f "$foundFiles" -z 2 -x IsmrmrdParameterMap_Siemens_NX.xsl --skipSyncData
 done
 
 for id in ${idsSpiral[@]}; do
     printf -v filepattern "meas_MID%05d" "${id}"
     echo $filepattern 
-    foundFiles=$(ls ${pathData}/${filepattern}*.dat)
+    foundFiles=$(ls "${pathData}"/${filepattern}*.dat)
     echo "Converting $foundFiles to mrd using IsmrmrdParameterMap_Siemens_NX_ReadinSpiral.xsl"
-    siemens_to_ismrmrd -f $foundFiles -z 2 -x IsmrmrdParameterMap_Siemens_NX_ReadinSpiral.xsl --skipSyncData
+    siemens_to_ismrmrd -f "$foundFiles" -z 2 -x IsmrmrdParameterMap_Siemens_NX_ReadinSpiral.xsl --skipSyncData
 done
