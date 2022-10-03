0b6fe1c00db43fea0d055c1dff02ab86d781cbff
Alexander Jaffray <alexander.jaffray@gmail.com>
2022-10-03 10:31:46 -0700

    add comments and docstrings to fieldMapEstimator.jl

--- utils/fieldMapEstimator.jl
+++ utils/fieldMapEstimator.jl
@@ -1,18 +1,41 @@
 using Flux
 
+"""
+ml_cost(x::Matrix{T},y::Matrix{Complex{T}},z::Matrix{Complex{T}}, β)
+Calculates the ML estimator cost between an estimated phase map and the underlying multi-echo scan data
+# Arguments
+* `x::Matrix{T}` - fieldmap estimate (in radians)
+* `y::Matrix{Complex{T}}` - Complex first-echo image data
+* `z::Matrix{Complex{T}}` - Complex second-echo image data
+* `β` - Regularization parameter controlling roughness penalty
+"""
 function ml_cost(x::Matrix{T},y::Matrix{Complex{T}},z::Matrix{Complex{T}}, β) where T
 
     Ψ = (sum(abs.(conj.(y) .* z) .* (1 .- cos.(angle.(z) .- angle.(y) .- x)),dims=[1,2]) + β * R(x))[1]
 
 end
 
+"""
+R(x::Matrix{T})
+Regularization function which penalizes roughness
+# Arguments
+* `x::Matrix{T}` - fieldmap estimate (in radians)
+"""
 function R(x::Matrix{T}) where T
 
     return T.(0.5) * sum(abs2,diff(x,dims=1),dims=[1,2]) + T.(0.5) * sum(abs2,diff(x,dims=2),dims=[1,2])
 
 end
 
-function pcg_ml_est_fieldmap(y,z,β) 
+"""
+pcg_ml_est_fieldmap(y::AbstractMatrix{Complex{T}},z::AbstractMatrix{Complex{T}},β) 
+Estimates the fieldmap using the formalism presented by Funai and Fessler (Regularized Field map Estimation, IEEE 2008)
+# Arguments
+* `y::AbstractMatrix{Complex{T}}` - Complex first-echo image data
+* `z::AbstractMatrix{Complex{T}}` - Complex second-echo image data
+* `β` - Regularization parameter controlling roughness penalty
+"""
+function pcg_ml_est_fieldmap(y::AbstractMatrix{Complex{T}},z::AbstractMatrix{Complex{T}},β) where T
 
     d = 1
     κ = conj.(y) .* z
@@ -48,6 +71,17 @@ function pcg_ml_est_fieldmap(y,z,β)
 end
 
 
+"""
+estimateB0Maps(imData,slices, TE1,TE2,β,isrotated)
+Processes 3D volume data as output from MRIReco.reconstruction to estimate fieldmaps using the method presented by Funai and Fessler
+# Arguments
+* `imData` - 5-D array with complex image data
+* `slices` - vector of slices to process (must be within range of 3rd dimension of imData)
+* `TE1` - Echo time 1 [ms]
+* `TE2` - Echo time 2 [ms]
+* `β` - Regularization parameter controlling roughness penalty
+* `isrotated` - Boolean controlling whether to rotate the B0 maps to match the images or not (legacy feature)
+"""
 function estimateB0Maps(imData,slices, TE1,TE2,β,isrotated)
 
     b0Maps = Complex.(zeros(size(imData)[1:3]))
