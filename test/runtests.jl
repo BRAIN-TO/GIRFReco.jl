b434859442a9b2f36268bde48e31941a55633d99
Zhe Tim Wu <tim.wuzhe@gmail.com>
2024-01-19 11:06:15 -0500

    Setup environment for UT and CI

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,9 @@
 using GIRFReco
 using Test
+using Scratch
+
+global const tmpdir = @get_scratch!("temp")
+@info "For the output of tests, please refer to the directory $tmpdir"
 
 include("testutils.jl")
 include("testintegration.jl")
